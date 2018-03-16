c103d248a54ca9a9fd34d5656ad836b4b3141865
Jesús Giganto Diez <Jesus.Giganto@tajamar365.com>
2018-03-16 19:29:17 +0100

    s

--- HospitalGame/js/CuentaAtras.js
+++ HospitalGame/js/CuentaAtras.js
@@ -1,34 +1,113 @@
-﻿function countdown(element, minutes, seconds) {
+﻿
+var interval1; var interval2; var interval3; var a; var b; var c;
+a = false; b = false; c = false; var num = 0; var nombre = ""; 
+var x = ""; var y = "";
+//var desplazamiento = ale
+
+
+
+
+function countdown(element, minutes, seconds) {
+
+    nombre = NombresDrop();
+    x = PosicionAleatoria();
+    y = PosicionAleatoria();
+    var drop = "<div id=" + nombre + "' class='ui-widget-header' style='position:relative;top:"+x+"px;left:"+y+"px'>";
+    drop += "<p> Drop here</p> </div>"; 
     // Fetch the display element
     var el = document.getElementById(element);
     // Set the timer
-    var interval = setInterval(function () {
-        if (seconds == 0) {
-            if (minutes == 0) {
-                (el.innerHTML = "STOP!");//AQUI SE EJECUTA ALGO CUANDO FINALIZA LA CUENTA ATRAS                
-                //clearInterval(interval); 22:00 Jesús hay que hacer una variable interval por cada reloj captura con un if quien llego
-                // a cero y que llame a countdown
-                //alert("dentro");
-                if (element == 'countdown1') { a = true; }
-                if (element == 'countdown2') { b = true;  }
-                if (element == 'countdown3') { c = true;  }
-                clickbutton(a, b, c);
-                return;
-            } else {
-                minutes--;
-                seconds = 60;
+    if (element == 'countdown1') {
+
+        interval1 = setInterval(function () {
+            if (seconds == 0) {
+                if (minutes == 0) {
+                    (el.innerHTML = "STOP!");//AQUI SE EJECUTA CÓDIGO CUANDO FINALIZA LA CUENTA ATRAS 
+                    $("#dinamico").append(drop);
+                    
+                    arrastrar();
+                    clearInterval(interval1);                   
+                    $("#timer1").click();
+                    return;
+                } else {
+                    minutes--;
+                    seconds = 60;
+                }
             }
-        }
 
-        if (minutes > 0) {
-            var minute_text = minutes + (minutes > 1 ? ' minutes' : ' minute');
-        } else {
-            var minute_text = '';
-        }
+            if (minutes > 0) {
+                var minute_text = minutes + (minutes > 1 ? ' minutes' : ' minute');
+            } else {
+                var minute_text = '';
+            }
 
-        var second_text = seconds > 1 ? '' : '';
+            var second_text = seconds > 1 ? '' : '';
 
-        el.innerHTML = minute_text + ' ' + seconds + ' ' + second_text + '';
-        seconds--;
-    }, 1000); 
+            el.innerHTML = minute_text + ' ' + seconds + ' ' + second_text + '';
+            seconds--;
+        }, 300);
+    }
+    if (element == 'countdown2') {
+
+        interval2 = setInterval(function () {
+            if (seconds == 0) {
+                if (minutes == 0) {
+                    (el.innerHTML = "STOP!");//AQUI SE EJECUTA ALGO CUANDO FINALIZA LA CUENTA ATRAS                
+                    clearInterval(interval2); //22:00 Jesús hay que hacer una variable interval por cada reloj captura con un if quien llego
+                    // a cero y que llame a countdown
+                    //alert("dentro");  
+                    //b = true;
+                    //clickbutton(a, b, c)
+                    $("#timer2").click();
+                    return;
+                } else {
+                    minutes--;
+                    seconds = 60;
+                }
+            }
+
+            if (minutes > 0) {
+                var minute_text = minutes + (minutes > 1 ? ' minutes' : ' minute');
+            } else {
+                var minute_text = '';
+            }
+
+            var second_text = seconds > 1 ? '' : '';
+
+            el.innerHTML = minute_text + ' ' + seconds + ' ' + second_text + '';
+            seconds--;
+        }, 1000);
+    }
+    if (element == 'countdown3') {
+
+        interval3 = setInterval(function () {
+            if (seconds == 0) {
+                if (minutes == 0) {
+                    (el.innerHTML = "STOP!");//AQUI SE EJECUTA ALGO CUANDO FINALIZA LA CUENTA ATRAS                
+                    clearInterval(interval3); //22:00 Jesús hay que hacer una variable interval por cada reloj captura con un if quien llego
+                    // a cero y que llame a countdown
+                    //alert("dentro");  
+                    //c = true;
+                    //clickbutton(a, b, c)
+                    $("#timer3").click();
+                    return;
+                } else {
+                    minutes--;
+                    seconds = 60;
+                }
+            }
+
+            if (minutes > 0) {
+                var minute_text = minutes + (minutes > 1 ? ' minutes' : ' minute');
+            } else {
+                var minute_text = '';
+            }
+
+            var second_text = seconds > 1 ? '' : '';
+
+            el.innerHTML = minute_text + ' ' + seconds + ' ' + second_text + '';
+            seconds--;
+        }, 1000);
+    }
+   
 }
